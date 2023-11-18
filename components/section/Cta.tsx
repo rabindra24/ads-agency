c29f1397819b5b4c2cea2e9c7d1571003523042e
rabin2403 <rabindra2402@gmail.com>
2023-11-18 23:59:12 +0530

    first commit

---
+++ components/section/Cta.tsx
@@ -0,0 +1,39 @@
+import { banner } from "@/public/images/images";
+import Image from "next/image";
+import React from "react";
+import { Button } from "../ui/button";
+
+const Cta = () => {
+  return (
+    <section
+      className=" body-font relative md:w-[90%] md:mx-auto mx-4 text-center "
+      id="cta"
+    >
+      <Image
+        src={banner}
+        width={1000}
+        height={400}
+        className="absolute -z-10 h-full top-0 w-full object-cover object-center rounded-3xl"
+        alt="call to action"
+      />
+      <div className="container px-5  mx-auto">
+        <div className="lg:w-2/3 flex flex-col  py-14 sm:flex-row sm:items-center items-start mx-auto max-md:space-y-5">
+          <h1 className="flex-grow sm:pr-16 text-5xl  text-white font-thin">
+            Elevate your brand Today!
+          </h1>
+          <div className="space-y-4">
+            <p className="text-white">
+              Ready to transform your digital dresence? Let &apos s create magic
+              together! book our services now!
+            </p>
+            <Button className="flex-shrink-0 bg-white text-black  border-0 py-2 px-8 rounded-full text-lg mt-10 sm:mt-0">
+              Book Call
+            </Button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Cta;
